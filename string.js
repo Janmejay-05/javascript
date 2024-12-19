0f88df7f4cdfdf6352d4632e847fefdd380fcb24
Janmejaysinh Zala <janmejayazala@gmail.com>
2024-12-19 17:49:12 +0530

    stringque

---
+++ string.js
@@ -0,0 +1,55 @@
+19;
+// let nam = "Rahul";
+// let count = 0;
+// for (let i = 0; i < nam.length; i++) {
+//   if (
+//     nam[i] === "a" ||
+//     nam[i] == "A" ||
+//     nam[i] === "e" ||
+//     nam[i] == "E" ||
+//     nam[i] === "i" ||
+//     nam[i] == "I" ||
+//     nam[i] === "o" ||
+//     nam[i] == "O" ||
+//     nam[i] === "u" ||
+//     nam[i] == "U"
+//   ) {
+//     count++;
+//   }
+// }
+// console.log(`there are ${count} vovels`);
+
+// 20
+// let str = "AB1234";
+// let str1 = [];
+// let j;
+// for (let i = str.length - 1, j = 0; i >= 0; i--, j++) {
+//   str1[j] = str[i];
+//   // console.log(str1);
+// }
+// console.log(str1.join(""));
+
+// 21
+
+// let str = "nitin";
+// let str1 = [];
+// let j;
+// for (let i = str.length - 1, j = 0; i >= 0; i--, j++) {
+//   str1[j] = str[i];
+//   // console.log(str1);
+// }
+// if (str1.join("") == str) {
+//   console.log("its pallindrom");
+// } else {
+//   console.log("its not");
+// }
+
+// 22
+// let str = "Hello World 2024";
+// const result = () => str.split(" ").join("");
+// console.log(result());
+
+// 23
+// let str = "JavaScript is fun";
+// const result = () => str.split(" ");
+// console.log(result());
